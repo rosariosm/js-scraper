ff54ee0e422217be934b3bb6c2f9b78fb0347ead
Rosario Santa Marina <rosariosantamarina1@gmail.com>
2019-03-01 12:06:28 -0300

    Add event listener Nerwork.RecievedData  in order to get the amount of data recieved for each resource loaded in a page

--- scraper.js
+++ scraper.js
@@ -3,10 +3,15 @@ const $ = require('cheerio');
 const url = 'https://www.lanacion.com.ar/';
 const fs = require('fs');
 
-const length = (html) => {
+const resources = {};
+var dependencies = []
+
+
+const total_length = (e, page) => {
+  return totalUncompressedBytes = Object.values(resources).reduce((a, n) => a + n, 0);
 }
 
-const build_dependencies = () => {
+const build_dependencies = (html) => {
 }
 
 const frequency = (html) => {
@@ -20,6 +25,22 @@ const write_results = () => {
 
 puppeteer.launch().then(async browser => {
   const page = await browser.newPage();
+
+  page._client.on('Network.dataReceived', (event) => {
+    const request = page._networkManager._requestIdToRequest.get(event.requestId);
+	if (request && request.url().startsWith('data:')) {
+	  return;
+	}
+	const url = request.url();
+	const length = event.dataLength;
+	if (url in resources) {
+	  resources[url] += length;
+	}
+	else {
+	  resources[url] = length;
+	}
+  });
+
   await page.goto(url);
   html = await page.content();
 
