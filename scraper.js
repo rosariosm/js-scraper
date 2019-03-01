61ef60a4f789014223a750b2612befabed0d71fc
Rosario Santa Marina <rosariosantamarina1@gmail.com>
2019-03-01 10:55:56 -0300

    Add func structure

--- scraper.js
+++ scraper.js
@@ -2,3 +2,26 @@ const puppeteer = require('puppeteer');
 const $ = require('cheerio');
 const url = 'https://www.lanacion.com.ar/';
 const fs = require('fs');
+
+const length = (html) => {
+}
+
+const build_dependencies = () => {
+}
+
+const frequency = (html) => {
+}
+
+const write_results = () => {
+}
+
+
+
+
+puppeteer.launch().then(async browser => {
+  const page = await browser.newPage();
+  await page.goto(url);
+  html = await page.content();
+
+  await browser.close();
+});
